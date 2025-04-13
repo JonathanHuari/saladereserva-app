c3ab3c630b268feff5ad4784ca467e4ca9199792
Jonathan Huari <44471663@continental.edu.pe>
2025-04-13 11:45:39 -0500

    feat: componente base para el registro de usuario

---
+++ src/pages/RegistroUsuario.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RegistroUsuario = () => {
+  return (
+    <div>
+      <h2>Registro de Usuario</h2>
+      {/* Aquí irá el formulario */}
+    </div>
+  );
+};
+
+export default RegistroUsuario;
